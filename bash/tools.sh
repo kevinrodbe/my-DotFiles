36bcc1eead773a49f43f7ef2f1342f7e842c57a2
kevinrodbe <kevin.rodriguez992@gmail.com>
2015-11-02 03:18:08 -0500

    flash y my apps

--- bash/tools.sh
+++ bash/tools.sh
@@ -16,7 +16,18 @@ sudo apt-get install libcurl3-openssl-dev
 
 sudo apt-get install pecl
 
+#Java
+#sudo add-apt-repository -y ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install oracle-java9-installer
+
 #sudo pecl install mongo
+
+#yo
+sudo apt-get install flashplugin-installer pepperflashplugin-nonfree
+sudo apt-get install adobe-flashplugin
+sudo apt-get install gimp inkscape
+sudo apt-get install bleachbit
 
 sudo apt-get install python-setuptools
 
