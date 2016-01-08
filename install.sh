8ac7b46645d0b6072ff84613f93087d779b0b994
kevinrodbe <kevin.rodriguez992@gmail.com>
2016-01-08 02:44:55 -0500

    chrome en install y phpmyadmin con carpeta

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ bash bash/zsh.sh
 bash bash/nodejs.sh
 bash bash/node_deps.sh
 bash bash/tools_linux.sh
-#bash bash/chrome.sh
+bash bash/chrome.sh
 #bash bash/firefox.sh
 bash bash/apache.sh
 bash bash/php.sh
 bash bash/mysql.sh
+mkdir /home/gohan/proyectos
 bash bash/phpmyadmin.sh
 bash bash/tools.sh
