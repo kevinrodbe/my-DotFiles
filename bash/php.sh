33869fcd4f0e7734b6bbee86c0948a1f99a12cd5
Kevin RodBe <kevin.rodriguez992@gmail.com>
2016-05-16 14:50:35 -0500

    update php7 total

--- bash/php.sh
+++ bash/php.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
+# para ver los modulos disponibles para php7
+# sudo apt-cache search php7-* 
 
-#sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
-sudo apt-get -y install php7.0 libapache2-mod-php7.0
+sudo apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mcrypt
 
 #instalar CURL para PHP
 sudo apt-get install curl
 
-sudo apt-get install php5-curl
+sudo apt-get install php7.0-curl
 
 #libreria
-sudo apt-get install php5-intl
-sudo apt-get install php5-gd
+sudo apt-get install php7.0-intl
+sudo apt-get install php7.0-gd
 
 sudo apt-get install php-pear
-sudo apt-get install php5-dev
+sudo apt-get install php7.0-dev
 
-sudo a2enmod php5
+#sudo a2enmod php5
+sudo apt-cache search php7-* 
 
 sudo apt-get install php-mbstring php7.0-mbstring
 
 sudo service apache2 restart
 
-echo "** php5 instalado **"
+echo "** php7 instalado **"
