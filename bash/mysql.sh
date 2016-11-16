3c70a7b62502ba2c37b5961647079099f5325a76
Kevin Rodríguez <kevinrodbe@users.noreply.github.com>
2016-11-16 03:25:50 -0500

    update mysql

--- bash/mysql.sh
+++ bash/mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install mysql-server mysql-client
-sudo apt-get install libapache2-mod-auth-mysql php5-mysql mysql-common
+sudo apt-get install php-mysql mysql-common
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 
