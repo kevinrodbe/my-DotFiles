36f882ff852d0f87dae1c30f15f7bec5deba55be
Kevin Rodríguez <kevinrodbe@users.noreply.github.com>
2016-11-16 03:55:26 -0500

    update

--- bash/update.sh
+++ bash/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade 
+#sudo apt-get dist-upgrade 
 sudo apt-get install build-essential chrpath libssl-dev libxft-dev
 
 echo "** actualización ok **"
