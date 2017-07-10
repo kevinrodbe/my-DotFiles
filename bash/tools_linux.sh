e1fbe211dbf10e9f711ab4382e970edbcba8ed31
Kevin Rodríguez <kevinrodbe@users.noreply.github.com>
2017-07-10 00:32:53 -0500

    feat(bash): added pip + emoji installation

--- bash/tools_linux.sh
+++ bash/tools_linux.sh
@@ -17,3 +17,19 @@ sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get install ubuntu-restricted-extras 
 
 echo "** tools_linux instalado **"
+
+# install pip3
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip   
+# verify installation 
+# pip --help                       
+pip install --upgrade pip
+
+
+#Enable Emoji firefox
+sudo add-apt-repository ppa:eosrei/fonts   
+sudo apt update && sudo apt install fonts-emojione-svginot
+
+#Emoji keyboard
+sudo apt install python3-gi gir1.2-gtk-3.0 gir1.2-glib-2.0 gir1.2-appindicator3-0.1 python3-xlib
+sudo pip3 install https://github.com/OzymandiasTheGreat/emoji-keyboard/archive/master.zip
