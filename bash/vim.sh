8702a388a695adea29f721d4b39d151023c31d95
kevinrodbe <kevin.rodriguez992@gmail.com>
2017-07-23 15:55:32 -0500

    feat(vim): adding vin configuration

--- bash/vim.sh
+++ bash/vim.sh
@@ -16,6 +16,10 @@ cd ~/.vim/autoload;
 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
 
+# install vim-plug
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install plugins
 cd ~/.vim/bundle
