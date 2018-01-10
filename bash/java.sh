bc73ee02d00ef52054728049b52e72fcf087b757
Kevin Rodríguez <kevinrodbe@users.noreply.github.com>
2018-01-10 16:51:44 -0500

    feat(bash): add java

---
+++ bash/java.sh
@@ -0,0 +1,9 @@
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+sudo apt-get upgrade --fix-missing   
+sudo apt-get install alien dpkg-dev debhelper build-essential 
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386   
+
+# do manual: https://developer.android.com/studio/index.html
+
+sdk install gradle 4.4.1
