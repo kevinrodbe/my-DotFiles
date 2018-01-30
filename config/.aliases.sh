715bbd8afd98daaf5d84cab6bfdc028576f2d08a
Kevin Rodríguez <kevinrodbe@users.noreply.github.com>
2018-01-30 15:41:54 -0500

    feat(config): add ionic aliases

--- config/.aliases.sh
+++ config/.aliases.sh
@@ -45,11 +45,14 @@ alias npmid="npm install -D"
 alias npmig="npm install -g"
 
 # ionic
+alias io="ionic"
+alias cdv="cordova"
 alias ioc="ionic cordova"
 alias iorm="rm -rf node_modules/ && rm -rf plugins/ && rm -rf www/ && rm -rf platforms/ && rm -rf .sourcemaps/"
 alias ioi="npm i && ioc build"
 alias ior="ioc run"
 alias ioan="ior android --device"
+alias cpl="cordova plugin list"
 
 #tmux
 alias klt="tmux kill-session"
